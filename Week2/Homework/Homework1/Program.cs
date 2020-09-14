b0354b439658e838a6b35d3e74123cc17ed36bab
A-Salty-Fish <1309032417@qq.com>
2020-09-14 09:20:54 +0800

    三角形

---
+++ Week2/Homework/Homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(3,4);
+            Point b = new Point(5, 6);
+            Point c = new Point(6, 6);
+
+            TriAngle triAngle = new TriAngle(a, b, c);
+            triAngle.ShowPoints();
+            Console.WriteLine(triAngle.GetArea());
+        }
+    }
+}
