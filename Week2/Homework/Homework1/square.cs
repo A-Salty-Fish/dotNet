758b6e2aa7714b821478a48aaea8f71de8b4f696
A-Salty-Fish <1309032417@qq.com>
2020-09-14 09:48:54 +0800

    正方形重载判断

--- Week2/Homework/Homework1/square.cs
+++ Week2/Homework/Homework1/square.cs
@@ -14,5 +14,20 @@ namespace Homework1
             PointsList[2] = c;
             PointsList[3] = d;
         }
+
+        public bool IsLegal()
+        {
+            double length = GetDist(PointsList[0].X, PointsList[0].Y, PointsList[1].X, PointsList[1].Y);
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = i + 1; j < 4; j++)
+                {
+                    if (GetDist(PointsList[i].X, PointsList[i].Y,
+                            PointsList[j].X, PointsList[j].Y) != length)
+                        return false;
+                }
+            }
+            return true;
+        }
     }
 }
