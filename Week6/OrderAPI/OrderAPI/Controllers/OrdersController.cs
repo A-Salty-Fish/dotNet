9a02c2a8ab91a509f62a81316c000be294cbf123
A-Salty-Fish <1309032417@qq.com>
2020-10-12 11:41:41 +0800

    WEB API Order 简陋版

---
+++ Week6/OrderAPI/OrderAPI/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+using OrderNameSpace;
+using DatabaseNameSpace;
+
+namespace OrderLibraryWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        private Database db = new Database();
+
+        [HttpGet]
+        public ActionResult<Order> GetById(long id)
+        {
+            return db.Orders.Find(id);
+        }
+
+        [HttpDelete]
+        public ActionResult<Order> Delete(long id)
+        {
+            var ret = db.Orders.Find(id);
+            if (ret == null) return null;
+            db.Orders.Remove(ret);
+            db.SaveChanges();
+            return ret;
+        }
+
+        [HttpPost]
+        public ActionResult<Order> Add(long itemid, string custom, double sum)
+        {
+            var ret = db.Add(new Order() { customer_name = custom, item_id = itemid, item_sum = sum });
+            db.SaveChanges();
+            return ret.Entity;
+        }
+    }
+}
---
+++ Week6/OrderAPI/OrderAPI/Data/database.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderNameSpace;
+
+namespace DatabaseNameSpace
+{
+    public class Database : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseMySql("server=localhost;database=eforder;user=root;password=");
+    }
+}
