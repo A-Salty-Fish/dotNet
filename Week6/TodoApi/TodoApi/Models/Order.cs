ae9db5d4cdf165ca26265dd30409bc22f6549d91
A-Salty-Fish <1309032417@qq.com>
2020-10-12 11:20:51 +0800

    上次的api

---
+++ Week6/TodoApi/TodoApi/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class Order//简单order
+    {
+        public int Order_Id { get; set; }
+        public string Name { get; set; }
+        public int Item_Id { get; set; }
+        public float Item_Sum { get; set; }
+    }
+}
